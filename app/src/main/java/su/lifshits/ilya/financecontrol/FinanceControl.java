3324d8e97824ff063b51a9a64694e6d2c9091d86
Lifshits I <ilya@lifshits.su>
2017-02-04 18:34:55 +0300

    Initial commit

---
+++ app/src/main/java/su/lifshits/ilya/financecontrol/FinanceControl.java
@@ -0,0 +1,13 @@
+package su.lifshits.ilya.financecontrol;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class FinanceControl extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_finance_control);
+    }
+}
